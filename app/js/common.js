6002993fcedf8c25668f9605ba0bde13920e632e
onyzhin <o.nyzhin@gmail.com>
2016-04-21 15:01:18 +0300

    add product page

--- app/js/common.js
+++ app/js/common.js
@@ -26,7 +26,38 @@ function slickInit(){
 		  slidesToShow: 1,
 		  slidesToScroll: 1,
 		  arrows: true,
-		  fade: true
+		  fade: true,		  
+		  centerMode: true
+		}); 
+	} 
+	if ($('.production-slider').length){
+		$('.production-slider').slick({
+		  	infinite: true,
+		  	slidesToShow: 5,
+		 	slidesToScroll: 5,
+		 	responsive: [
+		 	 {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3
+		      }
+		    },
+		 	{
+		      breakpoint: 768, 
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		 	{
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		    ] 
 		});
 	}
 	if ($('.slider-for').length){
