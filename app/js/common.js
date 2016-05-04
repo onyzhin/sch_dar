d71aa0d5fe615deaf4569b9801064a98df28a754
onyzhin <o.nyzhin@gmail.com>
2016-05-04 17:37:46 +0300

    opinion done

--- app/js/common.js
+++ app/js/common.js
@@ -198,15 +198,32 @@ function sendForm(){
 	}); 
 }
 
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+  var args = $.speed(duration, easing, complete);
+  var step = args.step;
+  return this.each(function(i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function(now) {
+      $.style(e, 'transform', 'rotate(' + now + 'deg)');
+      if (step) return step.apply(e, arguments);
+    };
+
+    $({deg: 0}).animate({deg: angle}, args);
+  });
+};
+
 $(document).ready(function(){
 	console.log('It is common.js'); 
 	navbartoggle();
 	slickInit();
 	fancybox();
 	footerplaceholder();
-	tabs();
+	tabs();var i=0;
+	$('.opinion-box-image').click(function(){ console.log(i++);
+		$(this).animateRotate(360, 2000);
+	})
 });
 
 $(window).resize(function(){
 	footerplaceholder();
-})
+}) 
